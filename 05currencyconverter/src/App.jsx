509ec2011b48d8551f4859754b1129fd9650a73a
viveknmg <vivek.suyal@nmgtechnologies.com>
2024-05-01 20:54:27 +0530

    05currencyconverter

--- 05currencyconverter/src/App.jsx
+++ 05currencyconverter/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import image from './assets/money.jpg';
+import { InputBox } from './components'
 function App() {
   const [count, setCount] = useState(0)
 
@@ -8,9 +9,9 @@ function App() {
       <div className='h-screen flex justify-center items-center bg-cover bg-no-repeat' style={{backgroundImage: `url(${image})`}}>
         <div className='flex backdrop-blur-sm border-2 border-white rounded-md'>
           <form >
-            <div></div>
+            <InputBox/>
             <div className='relative'>swap</div>
-            <div></div>
+            <InputBox/>
             <div className='mx-3 my-5 p-5 rounded-md bg-blue-600 text-white'><button type='submit' >USD TO INR</button></div>
           </form>
         </div>
--- 05currencyconverter/src/components/InputBox.jsx
+++ 05currencyconverter/src/components/InputBox.jsx
@@ -1,3 +1,15 @@
 const InputBox = ({})=>{
-    return 
+    return (
+        <>
+            <div className="bg-white flex rounded-md w-full">
+                <div className="flex">
+
+                </div>
+                <div className="flex">
+                    
+                </div>
+            </div>
+        </>
+    )
 }
+export default InputBox
