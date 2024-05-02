6f5bb17fca6e496140437eaff733cfa5c191e002
vivekhome <rishisuyal73362@gmail.com>
2024-05-03 01:09:20 +0530

    async await

--- 05currencyconverter/src/hooks/useConverter.js
+++ 05currencyconverter/src/hooks/useConverter.js
@@ -16,3 +16,30 @@ function useConverter(currency){
 }
 
 export default useConverter;
+
+// [ ----- Using async await ------]
+
+// import { useEffect, useState } from "react";
+
+// function useConverter(currency) {
+//     const [data, setData] = useState({});
+
+//     useEffect(() => {
+//         const fetchData = async () => {
+//             try {
+//                 const api = `https://open.er-api.com/v6/latest/${currency}`;
+//                 const response = await fetch(api);
+//                 const result = await response.json();
+//                 setData(result['rates'] || {});
+//             } catch (error) {
+//                 console.error("Error fetching data:", error);
+//             }
+//         };
+
+//         fetchData();
+//     }, [currency]);
+
+//     return data;
+// }
+
+// export default useConverter;
